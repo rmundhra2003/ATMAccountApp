3e53439f1369600c550a2074da54f4a9ef23a5bd
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-04-29 14:46:22 -0400

    initial commit

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -4,24 +4,49 @@ public class Account {
     private int accountNo;
     private String username;
     private String pin;
-    private int balance;
+    private float balance;
 
     public Account() {
 
     }
     //overloaded constructor
-    public Account(int acNo, String uname, String pin, int balance) {
+    public Account(int acNo, String uname, String pin, float balance) {
         this.accountNo = acNo;
         this.username = uname;
         this.pin = pin;
         this.balance = balance;
     }
 
+    //Define getters and setters for class members
+    public void setAccountno(int ac) {
+        this.accountNo = ac;
+    }
+    public int getAccountNo() {
+        return this.accountNo;
+    }
+    public void setUsername(String name) {
+        this.username = name;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+    public String getPin() {
+        return this.pin;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    public float getBalance() {
+        return this.balance;
+    }
     public void makeDeposit(int depositAmount) {
         this.balance += depositAmount;
 
     }
-    public String makeWithdrawal(int amount) {
+    public String makeWithdrawal(float amount) {
         String msg;
         if(amount <= this.balance) {
             this.balance -= amount;
