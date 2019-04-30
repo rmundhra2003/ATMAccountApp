0679e1f75074a443107c814b22f86d000aac4285
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-04-30 13:55:08 -0400

    Using hashmap for storing user accounts and files for I/O

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -11,14 +11,14 @@ package com.company;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Account {
     private int accountNo;
     private String username;
     private String pin;
     private float balance;
-    private ArrayList<Account> accountList;
+    private HashMap<Integer, Account> accountHashMap;
 
     public Account() {
 
@@ -58,49 +58,41 @@ public class Account {
     }
     public String checkBalance(int acNo, String value) {
         String s = "";
-        for (Account a : accountList) {
-            if (a.validateAccount(acNo, value)) {
-                s = "Current balance for account# "+ a.accountNo +" is " +a.balance;
-                break;
-            }
-        }
+        Account a = findAccountAndReturn(acNo, value);
+        if(a != null)
+            s = "Current balance for account# "+ a.accountNo +" is " +a.balance;
+        else
+            s = "Account not found\n";
+
         return s;
     }
     public String displayAcctInfo(int acNo, String value) {
         String s = "";
-        for (Account a : accountList) {
-            if (a.validateAccount(acNo, value)) {
-                s = "Current balance for account# "+ a.accountNo +" for  "+a.username +" is: $"  +a.balance;
-                break;
-            }
+        Account a = findAccountAndReturn(acNo, value);
+        if(a != null) {
+            s = "Current balance for account# "+ a.accountNo +" for  "+a.username +" is: $"  +a.balance;
         }
         return s;
     }
     public String makeDeposit(int acNo, String value, float depositAmount) {
         String s = "";
-        for (Account a : accountList) {
-            if (a.validateAccount(acNo, value)) {
-                a.balance += depositAmount;
-                s = "The balance is " +a.balance;
-                break;
-            }
+        Account a = findAccountAndReturn(acNo, value);
+        if(a != null) {
+            a.balance += depositAmount;
+            s = "The balance is " +a.balance;
         }
         return s;
     }
     public String makeWithdrawal(int acNo, String value, float amount) {
         String s = "";
-        for (Account a : accountList) {
-            if (a.validateAccount(acNo, value)) {
-                if(a.balance >= amount) {
-                    a.balance -= amount;
-                    s = "The balance is " + a.balance;
-                    break;
-                }else {
-                    s = "Insufficient funds to  process transaction balance is $" +a.balance;
-                    break;
-                }
+        Account a = findAccountAndReturn(acNo, value);
+        if(a != null) {
+            if (a.balance >= amount) {
+                a.balance -= amount;
+                s = "The balance is " + a.balance;
+            } else {
+                s = "Insufficient funds to  process transaction balance is $" + a.balance;
             }
-
         }
         return s;
     }
@@ -118,21 +110,50 @@ public class Account {
     }
     public boolean findAccount(int acNo, String value) {
         boolean found = false;
-        for (Account a : accountList) {
-            if (a.validateAccount(acNo, value)) {
+        Account ac1 = null;
+
+        Set set2 = accountHashMap.entrySet();
+        Iterator iterator2 = set2.iterator();
+
+        while (iterator2.hasNext()) {
+            Map.Entry mentry2 = (Map.Entry) iterator2.next();
+            acNo = (int)mentry2.getKey();
+            ac1 = (Account) mentry2.getValue();
+            if(ac1.validateAccount(acNo,value )) {
+                //Found account
                 found = true;
                 break;
             }
         }
         return found;
     }
+    //@Overloaded method
+    public Account findAccountAndReturn(int acNo, String value) {
+        Account ac1 = null;
+
+        Set set2 = accountHashMap.entrySet();
+        Iterator iterator2 = set2.iterator();
+
+        while (iterator2.hasNext()) {
+            Map.Entry mentry2 = (Map.Entry) iterator2.next();
+            acNo = (int)mentry2.getKey();
+            ac1 = (Account) mentry2.getValue();
+            if(ac1.validateAccount(acNo,value )) {
+                //Found account
+                break;
+            }
+        }
+        return ac1;
+    }
+
     public void readAccountsFromFile() {
         String filename = (System.getProperty("user.dir")+ File.separatorChar+"accounts.txt");
         System.out.println(filename);
 
         //Read the file
         BufferedReader reader = null;
-        accountList = new ArrayList<Account>();
+
+        accountHashMap = new HashMap<Integer, Account>();
         try {
             reader = new BufferedReader(new FileReader(filename));
             String line = reader.readLine();
@@ -141,7 +162,7 @@ public class Account {
                 //Populate the accounts array list
                 Account ac1 = new Account(Integer.valueOf(Integer.parseInt(acctStringAry[0])), acctStringAry[1], acctStringAry[2],
                         Float.valueOf(Float.parseFloat(acctStringAry[3])));
-                accountList.add(ac1);
+                accountHashMap.put(Integer.parseInt(acctStringAry[0]), ac1);
                 line = reader.readLine();
             }
             reader.close();
@@ -152,6 +173,9 @@ public class Account {
     public void writeAccountsToFile() {
         String filename = (System.getProperty("user.dir")+ File.separatorChar+"accounts.txt");
         System.out.println(filename);
+        Set set2 = accountHashMap.entrySet();
+        Iterator iterator2 = set2.iterator();
+
 
         PrintWriter writer = null;
         try {
@@ -160,8 +184,12 @@ public class Account {
             System.out.println("File does not exist!");
         }
         //Writing to file
-        for(Account acct:accountList) {
+        while (iterator2.hasNext()) {
+            Map.Entry mentry2 = (Map.Entry) iterator2.next();
+
+            Account acct = (Account) mentry2.getValue();
             writer.println(acct.getAccountNo() +" "+acct.getPin() +" " +acct.getUsername() +" " +acct.getBalance());
+
         }
         writer.close();
     }
